17e4c100efedda8649168c0a18f5e3057bda71a4
fudgenuggets12 <jsalcedo1102@gmail.com>
2020-06-23 15:33:13 -0500

    Update Main.java

--- src/Working/Main.java
+++ src/Working/Main.java
@@ -21,6 +21,7 @@ public class Main extends Application {
 	Button QuickSort = new Button("Quick Sort");
 	Button MergeSort = new Button("Merge Sort");
 	Button HeapRadixSort = new Button("Heap Radix Sort");
+	Button GenerateList = new Button("Generate List");
 	Label winningalgorithm = new Label("Winning Algorithm");
 	Label listProperties = new Label("List Properties");
 	Label results = new Label("Experimental Results");
@@ -65,6 +66,7 @@ public class Main extends Application {
 		g.add(QuickSort, 0, 2);
 		g.add(MergeSort, 0, 3);
 		g.add(HeapRadixSort, 0, 4);
+		g.add(GenerateList, 0, 5);
 		g.add(winningalgorithm, 1,0);
 		g.add(algorithm, 2, 0);
 		g.add(listProperties, 1, 1);
@@ -104,13 +106,77 @@ public class Main extends Application {
 		stage.setScene(scene);
 		stage.show();
 	}
+    //Generates a list with the number of elements and list properties specified.
+	public static int[] generateList(int numElements, char type) throws InterruptedException {
+		//Generates inorder list
+		int[] list = new int[numElements];
+		if(type == 'i')
+	    {
+			for(int x = 1; x<=numElements;x++)
+			{
+				list[x-1] = x;
+			}
+	    }
+		else if(type == 'r')
+	    {
+			for(int x = 1; x<=numElements;x++)
+			{
+				list[numElements-x] = x;
+			}
+	    }
+		else if(type == 'a')
+		{
+			//Generates inorder list
+			for(int x = 1; x<=numElements;x++)
+			{
+				list[x-1] = x;
+			}
+			//Generates random number between 2 and 8
+			int r = 2 + (int)(System.currentTimeMillis()%((8-2) + 1));
+			//Divides random number by 2 because 2 elements will be switched. a number between 4 and 1 is generated
+			int counter = r/2;
+			System.out.println(counter);
+			for(int x = 0; x<counter; x++)
+			{   
+				r = 0 + (int)(System.currentTimeMillis()%(((numElements-1)-0) + 1));
+				int s = r;
+				//Makes sure s is not equal to r ever
+			    while(s == r) {
+			    	s = 0 + (int)(System.currentTimeMillis()%(((numElements-1)-0) + 1));
+			    }
+			    int el = list[s];
+			    list[s] = list[r];
+			    list[r] = el;
+			}
+		}
+		else if(type == 'd')
+		{
+			for(int x = 0; x<numElements; x++)
+			{
+				//Generates random number between 1 and n to add to the array
+				int r = 1 + (int)(System.currentTimeMillis()%(((numElements)-1) + 1));
+				list[x] = r;
+				Thread.sleep(r);
+			}
+		}
+		return list;
+	}
 
-	
-	
-	public static void main(String args[])
+	public static void printList(int[] list)
+	{
+		for(int i: list)
+		{
+			System.out.print(i);
+			System.out.print(",");
+		}
+	}
+	public static void main(String args[]) throws InterruptedException
     {
     	
-    	Application.launch(args);
+    	//Application.launch(args);
+		int listSize = 60;
+		int[] list = generateList(listSize,'d');
+		printList(list);
 		
     	
     }
