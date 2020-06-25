4f37141fb4de422175328b0e3138abd972ece872
joehenriquez <31773428+joehenriquez@users.noreply.github.com>
2020-06-25 18:37:02 -0500

    Update MergeSort.java

--- src/Working/MergeSort.java
+++ src/Working/MergeSort.java
@@ -2,15 +2,23 @@ package Working;
 
 public class MergeSort {
 
- if(list.length>1){
+	public static void mergeSort(int[] list){
+		if(list.length>1){
 
- 	//merge sort first half
- 	int[] firsthalf = new int[list.length / 2];
- 	System.arraycopy(list, 0, firsthalf, 0, list.length / 2);
- 	MergeSort(firsthalf);
+ 			//merge sort first half
+ 			int[] firsthalf = new int[list.length / 2];
+ 			System.arraycopy(list, 0, firsthalf, 0, list.length / 2);
+ 			MergeSort(firsthalf);
 
- 	//merge sort the second half
- 	int secondHalfLength = list.length - list.length / 2;
- 	int[] secondHalf = new int[secondHalfLength];
- }
+ 			//merge sort the second half
+ 			int secondHalfLength = list.length - list.length / 2;
+ 			int[] secondHalf = new int[secondHalfLength];
+ 			System.arraycopy(list, list.length / 2,
+ 				secondHalf, 0, secondHalfLength);
+ 			merge(secondHalf);
+
+ 			//merge firstHalf with secondHalf into list
+ 			merge(firsthalf, secondHalf, list);
+ 		}
+	}
 }
